727941243b7cfb23060453f1f1c8ea6bb689a67e
Pratibha Chippa <pratibhachippa16@gmail.com>
2024-07-24 14:48:07 +0530

    Date - 21072024 programms

---
+++ src/ex_21072024/Lab107.java
@@ -0,0 +1,16 @@
+package ex_21072024;
+
+public class Lab107 {
+    public static void main(String[] args) {
+
+        int age  = 34;
+        int pratibha_chippa = 34;
+        String name = "Amit";
+
+        for (int pratibha=1; pratibha < 6; pratibha++) { // 1 to 5
+            System.out.println(pratibha); /// 5<5
+        }
+
+    }
+
+}
---
+++ src/ex_21072024/Lab121.java
@@ -0,0 +1,12 @@
+package ex_21072024;
+
+public class Lab121 {
+    public static void main(String[] args) {
+        System.out.println("Even no from 0 to 50 : ");
+        for (int i = 0; i <=50 ; i++) {
+            if(i%2 == 0){  //  if(i%2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
